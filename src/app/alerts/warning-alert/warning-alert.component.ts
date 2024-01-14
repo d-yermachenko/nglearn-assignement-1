837010e9506dfea4f49432f60f40499a816a0846
D Yermachenko <d.yermachenko@gmail.com>
2024-01-14 02:00:02 +0100

    Moving 1 templite to integrated into ts file

--- src/app/alerts/warning-alert/warning-alert.component.ts
+++ src/app/alerts/warning-alert/warning-alert.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
-  templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
-})
+  template: `
+      <div class="panel panel-warning">
+      <div class="panel-heading">{{ title }}</div>
+        <div class="panel-body">
+            <warning-alert-details></warning-alert-details>
+        </div>
+    </div>
+  `})
 export class WarningAlertComponent {
   public title: string = "Warning";
   public message: string = "Warning message";
